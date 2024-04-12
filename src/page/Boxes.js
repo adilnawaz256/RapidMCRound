5fe58ef579d7be07475c9190b971ebeff86ef936
adil-cendrol <adil.cendrol@gmail.com>
2024-04-12 11:36:26 +0530

    fixed

--- src/page/Boxes.js
+++ src/page/Boxes.js
@@ -2,28 +2,28 @@ import React from 'react'
 const Boxes = () => {
   return (
     <div className='boxes'>
-        <div className='box'>
+        <div className='box shadow-lg bg-white'>
           <div className='boxchild'>
             <p>Total</p>
             <h2 className=''>64</h2>
           </div>
           <div className='boxicon'><svg className='svg1' xmlns="http://www.w3.org/2000/svg" height="16px" width="16px" viewBox="0 0 448 512"><path d="M181.3 32.4c17.4 2.9 29.2 19.4 26.3 36.8L197.8 128h95.1l11.5-69.3c2.9-17.4 19.4-29.2 36.8-26.3s29.2 19.4 26.3 36.8L357.8 128H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H347.1L325.8 320H384c17.7 0 32 14.3 32 32s-14.3 32-32 32H315.1l-11.5 69.3c-2.9 17.4-19.4 29.2-36.8 26.3s-29.2-19.4-26.3-36.8l9.8-58.7H155.1l-11.5 69.3c-2.9 17.4-19.4 29.2-36.8 26.3s-29.2-19.4-26.3-36.8L90.2 384H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h68.9l21.3-128H64c-17.7 0-32-14.3-32-32s14.3-32 32-32h68.9l11.5-69.3c2.9-17.4 19.4-29.2 36.8-26.3zM187.1 192L165.8 320h95.1l21.3-128H187.1z"/></svg></div>
         </div>
-        <div className='box'>
+        <div className='box shadow-lg bg-white'>
           <div className='boxchild'>
             <p>Pending</p>
             <h2>64</h2>
           </div>
           <div className='boxicon'><svg className='svg2' xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg></div>
         </div>
-        <div className='box'>
+        <div className='box shadow-lg bg-white'>
           <div className='boxchild'>
             <p>Approved</p>
             <h2>64</h2>
           </div>
           <div className='boxicon'><svg className='svg3' xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg></div>
         </div>
-        <div className='box'>
+        <div className='box shadow-lg bg-white'>
           <div className='boxchild'>
           <p>Certification Pending</p>
           <h2>64</h2>
